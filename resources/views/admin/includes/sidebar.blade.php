d5614240a647c47cdb2dc19dbf9aa452a228c36b
Samnad Sainulabdeen <samnads2014@gmail.com>
2024-03-06 23:53:16 +0530

    templating

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -1,21 +1,7 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
-        <li class="nav-item nav-profile">
-            <a href="#" class="nav-link">
-                <div class="nav-profile-image">
-                    <img src="{{ asset('assets/admin/images/faces/face1.jpg') }}" alt="profile">
-                    <span class="login-status online"></span>
-                    <!--change to offline or busy as needed-->
-                </div>
-                <div class="nav-profile-text d-flex flex-column">
-                    <span class="font-weight-bold mb-2">Administrator</span>
-                    <span class="text-secondary text-small">Super Admin</span>
-                </div>
-                <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
-            </a>
-        </li>
         <li class="nav-item">
-            <a class="nav-link" href="{{ url('dashboard') }}">
+            <a class="nav-link" href="{{ url('admin/dashboard') }}">
                 <span class="menu-title">Dashboard</span>
                 <i class="mdi mdi-home menu-icon"></i>
             </a>
