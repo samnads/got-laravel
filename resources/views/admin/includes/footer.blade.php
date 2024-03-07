0493caf711d87e22835950e159d0b419d86f69be
Samnad Sainulabdeen <samnads2014@gmail.com>
2024-03-07 21:28:29 +0530

    Update footer.blade.php

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -2,6 +2,6 @@
     <div class="container-fluid d-flex justify-content-between">
         <span class="text-muted d-block text-center text-sm-start d-sm-inline-block">Copyright ©
             CloudVeins Technologies {{date('Y') }}</span>
-        <span class="float-none float-sm-end mt-1 mt-sm-0 text-end"> Made with ❤️ in India.</span>
+        <span class="float-none float-sm-end mt-1 mt-sm-0 text-end"> Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</span>
     </div>
 </footer>
