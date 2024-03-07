9ee5c0cd9f08d70bd1a5e1566b5b4256b3775986
Samnad Sainulabdeen <samnads2014@gmail.com>
2024-03-07 23:37:28 +0530

    Create AdminAuthController.php

---
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class AdminAuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->route('admin-dashboard');
+        }
+        return back()->withErrors([]);
+    }
+}
