8e2f8eb1e8b7e1f22cc4dfcba9c90abd9cddabe1
Samnad Sainulabdeen <samnads2014@gmail.com>
2024-03-07 23:23:37 +0530

    Update AdminLoginController.php

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -8,40 +8,9 @@ use Illuminate\Http\Request;
 
 class AdminLoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/admin/dashboard';
-    public function showLoginForm(Request $request)
-    {
-        return view('admin.login', []);
-    }
     public function login(Request $request)
     {
-        return view('admin.login', []);
-    }
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
+        dd('login');
+        //return view('admin.login', []);
     }
 }
