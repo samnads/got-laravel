dc69c494c5cc907a9c3142a9b2f0dc5b80bda9bb
Samnad Sainulabdeen <samnads2014@gmail.com>
2024-03-07 23:23:32 +0530

    Update AdminController.php

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -7,18 +7,10 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
     public function index(Request $request)
     {
-        return view('admin.dashboard', []);
+        return view('admin.login', []);
+        //return view('admin.dashboard', []);
     }
     public function dashboard(Request $request)
     {
