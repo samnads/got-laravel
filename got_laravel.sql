b584e6133f5dd9401c88f88e8d26e55f5680267c
Samnad S (Developer) <samnads2014@gmail.com>
2024-03-10 19:50:39 +0530

    Update got_laravel.sql

--- got_laravel.sql
+++ got_laravel.sql
@@ -7,6 +7,21 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
+DROP TABLE IF EXISTS `address_types`;
+CREATE TABLE `address_types` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `address_types` (`id`, `name`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1,	'Home',	'2024-03-10 16:52:56',	'2024-03-10 16:52:56',	NULL),
+(2,	'Office',	'2024-03-10 16:52:56',	'2024-03-10 16:52:56',	NULL),
+(3,	'Other',	'2024-03-10 16:52:56',	'2024-03-10 16:52:56',	NULL);
+
 DROP TABLE IF EXISTS `brands`;
 CREATE TABLE `brands` (
   `brand_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -18,6 +33,69 @@ CREATE TABLE `brands` (
   PRIMARY KEY (`brand_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE `customers` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `mobile_number_1_cc` varchar(10) NOT NULL DEFAULT '+91',
+  `mobile_number_1` varchar(100) NOT NULL,
+  `mobile_number_1_otp` int(20) DEFAULT NULL,
+  `mobile_number_1_otp_expired_at` datetime DEFAULT NULL,
+  `mobile_number_1_verified_at` datetime DEFAULT NULL COMMENT 'is otp verified ?',
+  `password` varchar(255) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  `device_type` varchar(100) DEFAULT NULL COMMENT 'android | ios',
+  `push_token` varchar(255) DEFAULT NULL,
+  `default_address_id` bigint(20) unsigned DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `mobile_number_1_cc_mobile_number_1` (`mobile_number_1_cc`,`mobile_number_1`),
+  KEY `default_address_id` (`default_address_id`),
+  CONSTRAINT `customers_ibfk_1` FOREIGN KEY (`default_address_id`) REFERENCES `customer_addresses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `customers` (`id`, `name`, `email`, `mobile_number_1_cc`, `mobile_number_1`, `mobile_number_1_otp`, `mobile_number_1_otp_expired_at`, `mobile_number_1_verified_at`, `password`, `token`, `device_type`, `push_token`, `default_address_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(7,	'CloudVeins Test',	'hr@example.com',	'+91',	'9745451448',	NULL,	'2024-03-10 14:18:04',	NULL,	NULL,	'$2y$12$Zqezx2MGCOPBSWmMc.8UcOxqR/3D18lVCGXz.rv4q9DbvK0Cc2cYW',	NULL,	NULL,	42,	'2024-03-10 12:55:24',	'2024-03-10 14:18:01',	NULL);
+
+DROP TABLE IF EXISTS `customer_addresses`;
+CREATE TABLE `customer_addresses` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `customer_id` bigint(20) unsigned NOT NULL,
+  `name` varchar(200) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `latitude` varchar(150) DEFAULT NULL,
+  `longitude` varchar(150) DEFAULT NULL,
+  `apartment_no` varchar(255) DEFAULT NULL,
+  `apartment_name` varchar(255) DEFAULT NULL,
+  `street` varchar(255) DEFAULT NULL,
+  `landmark` varchar(255) DEFAULT NULL,
+  `pin_code` varchar(15) DEFAULT NULL,
+  `mobile_no` varchar(100) DEFAULT NULL,
+  `address_type` bigint(20) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `address_type` (`address_type`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `customer_addresses_ibfk_1` FOREIGN KEY (`address_type`) REFERENCES `address_types` (`id`),
+  CONSTRAINT `customer_addresses_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `customer_addresses` (`id`, `customer_id`, `name`, `address`, `latitude`, `longitude`, `apartment_no`, `apartment_name`, `street`, `landmark`, `pin_code`, `mobile_no`, `address_type`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(34,	7,	'ghghgh',	'addressssssss',	'245454545',	'4545454545',	'103',	'nameeeeee',	NULL,	'Near 5th milestone',	NULL,	'855256454',	1,	'2024-03-10 13:32:50',	'2024-03-10 13:50:07',	'2024-03-10 13:50:07'),
+(35,	7,	'trtrt',	'Test Address',	'245454545',	'4545454545',	'103',	'Cyber Zone',	NULL,	'Near 5th milestone',	NULL,	'8552564545',	1,	'2024-03-10 13:34:46',	'2024-03-10 13:34:46',	NULL),
+(36,	7,	'trtrt',	'Test Address',	'245454545',	'4545454545',	'103',	'Cyber Zone',	NULL,	'Near 5th milestone',	NULL,	'8552564545',	1,	'2024-03-10 13:34:54',	'2024-03-10 13:50:50',	'2024-03-10 13:50:50'),
+(37,	7,	'trtrt',	'Test Address',	'245454545',	'4545454545',	'103',	'Cyber Zone',	NULL,	'Near 5th milestone',	NULL,	'8552564545',	1,	'2024-03-10 13:36:35',	'2024-03-10 13:36:35',	NULL),
+(38,	7,	'trtrt',	'Test Address',	'245454545',	'4545454545',	'103',	'Cyber Zone',	NULL,	'Near 5th milestone',	NULL,	'8552564545',	1,	'2024-03-10 13:36:40',	'2024-03-10 13:36:40',	NULL),
+(39,	7,	'trtrt',	'Test Address',	'245454545',	'4545454545',	'103',	'Cyber Zone',	NULL,	'Near 5th milestone',	NULL,	'8552564545',	1,	'2024-03-10 13:36:44',	'2024-03-10 13:49:49',	'2024-03-10 13:49:49'),
+(40,	7,	'trtrt',	'Test Address',	'245454545',	'4545454545',	'103',	'Cyber Zone',	NULL,	'Near 5th milestone',	NULL,	'8552564545',	1,	'2024-03-10 13:49:31',	'2024-03-10 13:49:31',	NULL),
+(41,	7,	'ghghgh',	'addressssssss',	'245454545',	'4545454545',	'103',	'nameeeeee',	NULL,	'Near 5th milestone',	NULL,	'855256454',	1,	'2024-03-10 13:52:23',	'2024-03-10 13:52:43',	NULL),
+(42,	7,	'trtrt',	'Test Address',	'245454545',	'4545454545',	'103',	'Cyber Zone',	NULL,	'Near 5th milestone',	NULL,	'8552564545',	1,	'2024-03-10 14:18:01',	'2024-03-10 14:18:01',	NULL);
 
 DROP TABLE IF EXISTS `districts`;
 CREATE TABLE `districts` (
@@ -192,7 +270,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `users` (`id`, `name`, `username`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1,	'Admin',	'admin@example.com',	'admin@example.com',	'2024-03-07 10:49:50',	'$2y$12$SjmwFDIffWTH4X84sgvv/.VFWHJdaCOH9i0P3s/mx.p8cqWSRHvIm',	'SChWyX9YnmTYdWIjw7dopA1WfZw4nBSJjyyfvFnp68t52nX07ihx4S7XGETj',	'2024-03-07 10:49:50',	'2024-03-07 10:49:50',	NULL);
+(1,	'Admin',	'admin@example.com',	'admin@example.com',	'2024-03-07 10:49:50',	'$2y$12$SjmwFDIffWTH4X84sgvv/.VFWHJdaCOH9i0P3s/mx.p8cqWSRHvIm',	'tzuOJJpa6co6LK9DvedtTcFNPSyH7Tq0XLNlNLBcoP4mODz5z7npcxNSUfYR',	'2024-03-07 10:49:50',	'2024-03-07 10:49:50',	NULL);
 
 DROP TABLE IF EXISTS `vendors`;
 CREATE TABLE `vendors` (
@@ -227,4 +305,4 @@ CREATE TABLE `vendors` (
 INSERT INTO `vendors` (`id`, `vendor_name`, `owner_name`, `gst_number`, `pan_number`, `mobile_number`, `country_id`, `state_id`, `district_id`, `location_id`, `address`, `latitude`, `longitude`, `accuracy`, `shop_thumbnail`, `username`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES
 (1,	'Vendor XYZ',	'rer',	'erer',	'f',	're',	'ff',	'ds',	'df',	'd',	'sds',	'dsds',	'dsd',	'sds',	'dsd',	'vendor@example.com',	'vendor@example.com',	'2024-03-07 19:50:15',	'$2y$12$SjmwFDIffWTH4X84sgvv/.VFWHJdaCOH9i0P3s/mx.p8cqWSRHvIm',	'fPL7iVuEHEwppGsoeWPcuEjiiBbgJx2y4Twe2rTcqUTmQ4ITsiuXKGK3bfIv',	NULL,	NULL,	NULL);
 
--- 2024-03-09 13:21:56
+-- 2024-03-10 14:20:27
