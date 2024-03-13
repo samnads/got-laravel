9d4e590e4a5b98c1c1d00a25b870604e2e6f2d55
Samnad Sainulabdeen <samnads2014@gmail.com>
2024-03-14 00:12:24 +0530

    Update CartController.php

--- app/Http/Controllers/Customer/Api/CartController.php
+++ app/Http/Controllers/Customer/Api/CartController.php
@@ -10,13 +10,14 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Vendor;
 use App\Models\VendorProduct;
+use App\Models\Cart;
 use App\Models\ProductCategories;
 use Illuminate\Support\Str;
 use Response;
 
 class CartController extends Controller
 {
-    public function vendor_products(Request $request)
+    public function get_cart(Request $request)
     {
         /***************************************************************************************************** */
         $input = $request->all();
@@ -24,12 +25,10 @@ class CartController extends Controller
             (array) $input,
             [
                 'vendor_id' => 'required',
-                'sub_category_id' => 'required|integer',
             ],
             [],
             [
                 'vendor_id' => 'Vendor ID',
-                'sub_category_id' => 'Sub Category ID',
             ]
         );
         if ($validator->fails()) {
@@ -54,135 +53,39 @@ class CartController extends Controller
             ];
             return Response::json($response, 200, [], JSON_PRETTY_PRINT);
         }
-        if (@$request->sub_category_id) {
-            $sub_category = ProductCategories::where([['id', '=', $request->sub_category_id], ['parent_id', '!=', null]])->first();
-            $category = ProductCategories::where([['id', '=', $sub_category->parent_id]])->first();
-        }
         $vendor = Vendor::select(
             'id',
             'vendor_name as name',
         )
             ->where([['id', '=', $request->vendor_id]])
             ->first();
-        $vendor_products = VendorProduct::select(
-            'p.id',
-            'p.name',
-            'p.code',
-            'p.description',
-            'vendor_products.maximum_retail_price',
-            'vendor_products.retail_price'
+        $cart_products = Cart::select(
+            'cart.vendor_product_id as id',
+            'p.name as name',
+            'p.code as code',
+            'p.description as description',
+            DB::raw('null as thumbnail_url'),
+            'cart.quantity',
+            'vp.maximum_retail_price',
+            'vp.retail_price'
         )
+            ->leftJoin('vendor_products as vp', function ($join) {
+                $join->on('cart.vendor_product_id', '=', 'vp.id');
+            })
             ->leftJoin('products as p', function ($join) {
-                $join->on('vendor_products.product_id', '=', 'p.id');
-            });
-        if (@$request->sub_category_id) {
-            $vendor_products->where([['p.product_sub_category_id', '=', $request->sub_category_id]]);
-        }
-        $vendor_products = $vendor_products->where([['vendor_products.vendor_id', '=', $request->vendor_id], ['p.deleted_at', '=', null]])
+                $join->on('vp.product_id', '=', 'p.id');
+            })
+            ->where([['vp.vendor_id', '=', $request->vendor_id], ['vp.deleted_at', '=', null], ['p.deleted_at', '=', null]])
             ->get();
         $response = [
             'status' => [
                 'success' => 'true',
                 'hasdata' => 'true',
-                'message' => 'Vendor products fetched successfully !',
+                'message' => 'Cart fetched successfully !',
             ],
             'data' => [
                 'vendor' => $vendor,
-                'category' => [
-                    'id' => @$category->id,
-                    'name' => @$category->name
-                ],
-                'sub_category' => [
-                    'id' => @$sub_category->id,
-                    'name' => @$sub_category->name
-                ],
-                'products' => $vendor_products
-
-            ]
-        ];
-        return Response::json($response, 200, [], JSON_PRETTY_PRINT);
-    }
-    public function cart(Request $request)
-    {
-        /***************************************************************************************************** */
-        $input = $request->all();
-        $validator = Validator::make(
-            (array) $input,
-            [
-                'vendor_id' => 'required',
-                'sub_category_id' => 'required|integer',
-            ],
-            [],
-            [
-                'vendor_id' => 'Vendor ID',
-                'sub_category_id' => 'Sub Category ID',
-            ]
-        );
-        if ($validator->fails()) {
-            $response = [
-                'status' => [
-                    'success' => 'false',
-                    'hasdata' => 'false',
-                    'message' => $validator->errors()->first()
-                ]
-            ];
-            return Response::json($response, 200, [], JSON_PRETTY_PRINT);
-        }
-        /***************************************************************************************************** */
-        $vendor = Vendor::find($input['vendor_id']);
-        if (!$vendor) {
-            $response = [
-                'status' => [
-                    'success' => 'false',
-                    'hasdata' => 'false',
-                    'message' => 'Vendor not found !',
-                ],
-            ];
-            return Response::json($response, 200, [], JSON_PRETTY_PRINT);
-        }
-        if (@$request->sub_category_id) {
-            $sub_category = ProductCategories::where([['id', '=', $request->sub_category_id], ['parent_id', '!=', null]])->first();
-            $category = ProductCategories::where([['id', '=', $sub_category->parent_id]])->first();
-        }
-        $vendor = Vendor::select(
-            'id',
-            'vendor_name as name',
-        )
-            ->where([['id', '=', $request->vendor_id]])
-            ->first();
-        $vendor_products = VendorProduct::select(
-            'p.id',
-            'p.name',
-            'p.code',
-            'p.description',
-            'vendor_products.maximum_retail_price',
-            'vendor_products.retail_price'
-        )
-            ->leftJoin('products as p', function ($join) {
-                $join->on('vendor_products.product_id', '=', 'p.id');
-            });
-        if (@$request->sub_category_id) {
-            $vendor_products->where([['p.product_sub_category_id', '=', $request->sub_category_id]]);
-        }
-        $vendor_products = $vendor_products->where([['vendor_products.vendor_id', '=', $request->vendor_id], ['p.deleted_at', '=', null]])
-            ->get();
-        $response = [
-            'status' => [
-                'success' => 'true',
-                'hasdata' => 'true',
-                'message' => 'Vendor products fetched successfully !',
-            ],
-            'data' => [
-                'vendor' => $vendor,
-                'category' => [
-                    'id' => @$category->id,
-                    'name' => @$category->name
-                ],
-                'sub_category' => [
-                    'id' => @$sub_category->id,
-                    'name' => @$sub_category->name
-                ],
-                'products' => $vendor_products
+                'cart_products' => $cart_products
 
             ]
         ];
