2ce5344684a323cfcfd8e13d356b698be3d25748
Samnad Sainulabdeen <samnads2014@gmail.com>
2024-03-14 01:09:37 +0530

    Update CartController.php

--- app/Http/Controllers/Customer/Api/CartController.php
+++ app/Http/Controllers/Customer/Api/CartController.php
@@ -91,4 +91,110 @@ class CartController extends Controller
         ];
         return Response::json($response, 200, [], JSON_PRETTY_PRINT);
     }
+    public function update_cart(Request $request)
+    {
+        /***************************************************************************************************** */
+        $input = $request->all();
+        $validator = Validator::make(
+            (array) $input,
+            [
+                'vendor_id' => 'required|integer',
+                'product_id' => 'required|integer',
+                'quantity' => 'required|integer',
+            ],
+            [],
+            [
+                'vendor_id' => 'Vendor ID',
+                'product_id' => 'Product ID',
+                'quantity' => 'Quantity',
+            ]
+        );
+        if ($validator->fails()) {
+            $response = [
+                'status' => [
+                    'success' => 'false',
+                    'hasdata' => 'false',
+                    'message' => $validator->errors()->first()
+                ]
+            ];
+            return Response::json($response, 200, [], JSON_PRETTY_PRINT);
+        }
+        /***************************************************************************************************** */
+        $vendor = Vendor::select(
+            'id',
+            'vendor_name as name',
+        )
+            ->where([['id', '=', $request->vendor_id]])
+            ->first();
+        if (!$vendor) {
+            $response = [
+                'status' => [
+                    'success' => 'false',
+                    'hasdata' => 'false',
+                    'message' => 'Vendor not found !',
+                ],
+            ];
+            return Response::json($response, 200, [], JSON_PRETTY_PRINT);
+        }
+        /***************************************************************************************************** */
+        switch ($request->quantity) {
+            case '0':
+            case null:
+                $item = Cart::where([['customer_id', '=', $request->id], ['vendor_product_id', '=', $request->product_id]])->withTrashed()->first();
+                if ($item) {
+                    // update
+                    $item->delete();
+                }
+                break;
+            default:
+                $item = Cart::where([['customer_id', '=', $request->id], ['vendor_product_id', '=', $request->product_id]])->withTrashed()->first();
+                if ($item) {
+                    // update
+                    $item->quantity = $request->quantity;
+                    $item->updated_at = now();
+                    $item->deleted_at = now();
+                } else {
+                    // create
+                    $item = new Cart();
+                    $item->customer_id = $request->id;
+                    $item->vendor_product_id = $request->product_id;
+                    $item->quantity = $request->quantity;
+                }
+                $item->save();
+                break;
+        }
+        /***************************************************************************************************** */
+        $cart_products = Cart::select(
+            'cart.vendor_product_id as id',
+            'p.name as name',
+            'p.code as code',
+            'p.description as description',
+            DB::raw('null as thumbnail_url'),
+            'cart.quantity',
+            'vp.maximum_retail_price',
+            'vp.retail_price'
+        )
+            ->leftJoin('vendor_products as vp', function ($join) {
+                $join->on('cart.vendor_product_id', '=', 'vp.id');
+            })
+            ->leftJoin('products as p', function ($join) {
+                $join->on('vp.product_id', '=', 'p.id');
+            })
+            ->where([['vp.vendor_id', '=', $request->vendor_id], ['vp.deleted_at', '=', null], ['p.deleted_at', '=', null]])
+            ->get();
+        /***************************************************************************************************** */
+        $response = [
+            'status' => [
+                'success' => 'true',
+                'hasdata' => 'true',
+                'message' => 'Cart updated successfully !',
+            ],
+            'data' => [
+                'vendor' => $vendor,
+                'cart_products' => $cart_products
+
+            ]
+        ];
+        return Response::json($response, 200, [], JSON_PRETTY_PRINT);
+    }
 }
