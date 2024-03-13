07123317903954c85153ac17bc5192c8e4a703cb
Samnad Sainulabdeen <samnads2014@gmail.com>
2024-03-13 20:18:15 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,7 @@ Route::prefix('customer')->group(function () {
     Route::post('verify', [LoginController::class, 'login_otp_verify']);
     Route::group(['middleware' => 'customerApiTokenCheck'], function () {
         Route::post('update_profile', [ProfileController::class, 'basic_data_entry']);
+        Route::post('profile', [ProfileController::class, 'get_profile']);
         Route::post('save_address', [AddressController::class, 'create_address']);
         Route::post('update_address', [AddressController::class, 'update_address']);
         Route::post('set_default_address', [AddressController::class, 'update_default_address']);
