97ec52c421234e11f71ba16cac9854d86fc6ab30
Samnad Sainulabdeen <samnads2014@gmail.com>
2024-04-17 01:49:58 +0530

    Update header.blade.php

--- resources/views/shop/includes/header.blade.php
+++ resources/views/shop/includes/header.blade.php
@@ -5,18 +5,18 @@
             <div class="mobile-toggle-menu"><i class='bx bx-menu'></i>
             </div>
 
-            <div class="search-bar d-lg-block d-none" data-bs-toggle="modal" data-bs-target="#SearchModal">
+            <!--<div class="search-bar d-lg-block d-none" data-bs-toggle="modal" data-bs-target="#SearchModal">
                 <a href="avascript:;" class="btn d-flex align-items-center"><i class='bx bx-search'></i>Search</a>
-            </div>
+            </div>-->
 
 
             <div class="top-menu ms-auto">
                 <ul class="navbar-nav align-items-center gap-1">
-                    <li class="nav-item mobile-search-icon d-flex d-lg-none" data-bs-toggle="modal"
+                    <!--<li class="nav-item mobile-search-icon d-flex d-lg-none" data-bs-toggle="modal"
                         data-bs-target="#SearchModal">
                         <a class="nav-link" href="avascript:;"><i class='bx bx-search'></i>
                         </a>
-                    </li>
+                    </li>-->
                     <!--<li class="nav-item dropdown dropdown-laungauge d-none d-sm-flex">
                         <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="avascript:;"
                             data-bs-toggle="dropdown"><img src="{{ asset('assets/vendor/images/county/02.png') }}"
@@ -648,8 +648,8 @@
                     <img src="{{ asset('assets/vendor/images/avatars/avatar.jpg') }}" class="user-img"
                         alt="user avatar">
                     <div class="user-info">
-                        <p class="user-name mb-0">{{Auth::guard('vendor')->user()->vendor_name}}</p>
-                        <p class="designattion mb-0">[ {{Auth::guard('vendor')->user()->username}} ]</p>
+                        <p class="user-name mb-0">{{ Auth::guard('vendor')->user()->vendor_name }}</p>
+                        <p class="designattion mb-0">[ {{ Auth::guard('vendor')->user()->username }} ]</p>
                     </div>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end">
@@ -671,7 +671,8 @@
                     <li>
                         <div class="dropdown-divider mb-0"></div>
                     </li>-->
-                    <li><a class="dropdown-item d-flex align-items-center" href="{{ route('vendor.do-logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i
+                    <li><a class="dropdown-item d-flex align-items-center" href="{{ route('vendor.do-logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i
                                 class="bx bx-log-out-circle"></i><span>Logout</span></a>
                     </li>
                 </ul>
@@ -681,5 +682,5 @@
 </header>
 <!--end header -->
 <form id="logout-form" action="{{ route('vendor.do-logout') }}" method="POST" class="d-none">
-        @csrf
-    </form>
+    @csrf
+</form>
