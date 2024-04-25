5954abf56a79268bfd9053f6a4d8d2b72d756724
Samnad Sainulabdeen <samnads2014@gmail.com>
2024-04-26 00:53:16 +0530

    new

--- app/Http/Controllers/User/UserProductCategoryController.php
+++ app/Http/Controllers/User/UserProductCategoryController.php
@@ -3,9 +3,73 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProductCategories;
 use Illuminate\Http\Request;
+use DB;
 
 class UserProductCategoryController extends Controller
 {
-    //
+    public function categories_list(Request $request)
+    {
+        if ($request->ajax()) {
+            try {
+                switch ($request->action) {
+                    case 'datatable':
+                        $data_table['draw'] = $request->draw;
+                        $rows = ProductCategories::select(
+                            'product_categories.id',
+                            'product_categories.parent_id',
+                            'product_categories.name',
+                            'product_categories.description',
+                            'product_categories.thumbnail_image'
+                        );
+                        $data_table['recordsTotal'] = $rows->count();
+                        $rows->where(function ($query) use ($request) {
+                            $query->where([['product_categories.name', 'LIKE', "%{$request->search['value']}%"]]);
+                            $query->orWhere([['product_categories.description', 'LIKE', "%{$request->search['value']}%"]]);
+                        });
+                        if (@$request->order[0]['name']) {
+                            $rows->orderBy($request->order[0]['name'], $request->order[0]['dir']);
+                        } else {
+                            $rows->orderBy('product_categories.id', 'desc');
+                        }
+                        $data_table['recordsFiltered'] = $rows->count();
+                        $data_table['data'] = $rows->offset($request->start)->limit($request->length)->get()->toArray();
+                        foreach ($data_table['data'] as $key => $row) {
+                            $data_table['data'][$key]['slno'] = $key + 1;
+                            $data_table['data'][$key]['thumbnail_image_html'] = '<img src="'.config('url.uploads_cdn') . 'categories/' . ($row['thumbnail_image'] ?: 'default.jpg').'" class="product-img-2" alt="product img">';
+                            $data_table['data'][$key]['actions_html'] = '<div class="btn-group btn-group-sm bg-light" role="group" aria-label="Basic example">
+											<button type="button" data-action="quick-edit" data-id="' . $row['id'] . '" class="btn btn-outline-primary"><i class="bx bx-pencil"></i>
+											</button>
+										</div>';
+                        }
+                        return response()->json($data_table, 200, [], JSON_PRETTY_PRINT);
+                    default:
+                        $response = [
+                            'status' => false,
+                            'error' => [
+                                'type' => 'error',
+                                'title' => 'Error !',
+                                'content' => 'Unknown action.'
+                            ]
+                        ];
+                }
+                return response()->json($response, 200, [], JSON_PRETTY_PRINT);
+            } catch (\Exception $e) {
+                if (DB::transactionLevel() > 0) {
+                    DB::rollback();
+                }
+                $response = [
+                    'status' => false,
+                    'error' => [
+                        'type' => 'error',
+                        'title' => 'Error !',
+                        'content' => $e->getMessage()
+                    ]
+                ];
+                return response()->json($response, 200, [], JSON_PRETTY_PRINT);
+            }
+        }
+        return view('user.categories.categories-list', []);
+    }
 }
---
+++ public/assets/user/js/masters.categories.list-all.js
@@ -0,0 +1,207 @@
+loading_button_html = "Please wait...";
+let datatable = new DataTable('#datatable', {
+    processing: true,
+    serverSide: true,
+    autoWidth: true,
+    "order": [],
+    'ajax': {
+        dataType: "JSON",
+        'url': _url,
+        'data': function (data) {
+            data.action = 'datatable';
+            data.filter_order_status_id = $('select[name="filter_order_status_id"]').val();
+        },
+        "complete": function (json, type) { // data sent from controllerr
+            let response = json.responseJSON;
+            if (response.status == false) {
+                //toast(response.error.title, response.error.content, response.error.type);
+            }
+        },
+        "beforeSend": function () {
+            //Pace.restart();
+        },
+        "complete": function () {
+            //Pace.stop();
+        }
+    },
+    columns: [
+        { data: 'slno', name: 'slno' },
+        { data: 'thumbnail_image_html', name: 'thumbnail_image_html' },
+        { data: 'name', name: 'name' },
+        { data: 'description', name: 'description' },
+        { data: 'actions_html', name: 'actions_html' },
+    ],
+    columnDefs: [
+        {
+            targets: 'slno:name',
+            sortable: false,
+            type: 'html'
+        },
+        {
+            targets: 'thumbnail_image_html:name',
+            sortable: false,
+            type: 'html',
+            width: 1
+        },
+        {
+            targets: 'actions_html:name',
+            sortable: false,
+            type: 'num',
+            width: 1
+        },
+    ],
+    drawCallback: function (settings) {
+        rowEditListener();
+        $('[data-bs-toggle="tooltip"]').tooltip({
+            trigger: 'hover'
+        });
+    },
+});
+function rowEditListener() {
+    $('[data-action="edit-row"]').click(function () {
+        let id = this.getAttribute('data-id');
+        $('[name="id"]', edit_delivery_person_form).val(id);
+        $.ajax({
+            type: 'GET',
+            url: _base_url + "masters/delivery-persons",
+            dataType: 'json',
+            data: {
+                id: id,
+                action: "quick-edit-popup"
+            },
+            success: function (response) {
+                if (response.status == true) {
+                    edit_delivery_person_form_validator.resetForm();
+                    edit_delivery_person_form.trigger("reset");
+                    $('input[name="name"]', edit_delivery_person_form).val(response.data.vendor_delivery_person.name);
+                    $('input[name="mobile_number_1"]', edit_delivery_person_form).val(response.data.vendor_delivery_person.mobile_number_1);
+                    edit_delivery_person_modal.show();
+                } else {
+                    toastStatusFalse(response);
+                }
+            },
+            error: function (response) {
+                ajaxError(response);
+            },
+        });
+    });
+}
+$('[data-action="dt-refresh"]').click(function () {
+    datatable.draw();
+});
+$('[data-action="new-delivery-person"]').click(function () {
+    new_delivery_person_form_validator.resetForm();
+    new_delivery_person_form.trigger("reset");
+    new_delivery_person_modal.show();
+});
+$(document).ready(function () {
+    new_delivery_person_form_validator = new_delivery_person_form.validate({
+        focusInvalid: true,
+        ignore: [],
+        rules: {
+            "name": {
+                required: true,
+            },
+            "mobile_number_1": {
+                required: true,
+            },
+        },
+        messages: {},
+        errorPlacement: function (error, element) {
+            error.insertAfter(element.parent());
+        },
+        submitHandler: function (form) {
+            let submit_btn = $('button[type="submit"]', form);
+            submit_btn.html(loading_button_html).prop("disabled", true);
+            $.ajax({
+                type: 'POST',
+                url: _base_url + "masters/delivery-persons",
+                dataType: 'json',
+                headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') },
+                data: new_delivery_person_form.serialize(),
+                success: function (response) {
+                    if (response.status == true) {
+                        new_delivery_person_modal.hide();
+                        submit_btn.html('Save').prop("disabled", false);
+                        Swal.fire({
+                            title: response.message.title,
+                            text: response.message.content,
+                            icon: response.message.type,
+                            confirmButtonColor: swal_colors.success_ok,
+                            confirmButtonText: "OK",
+                            allowOutsideClick: false,
+                            didOpen: () => Swal.getConfirmButton().blur()
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                datatable.ajax.reload(null, false);
+                            }
+                        });
+                    } else {
+                        toastStatusFalse(response, { stack: 1 });
+                        submit_btn.html('Save').prop("disabled", false);
+                    }
+                },
+                error: function (response) {
+                    submit_btn.html('Save').prop("disabled", false);
+                    datatable.ajax.reload(null, false);
+                },
+            });
+        }
+    });
+    edit_delivery_person_form_validator = edit_delivery_person_form.validate({
+        focusInvalid: true,
+        ignore: [],
+        rules: {
+            "id": {
+                required: true,
+            },
+            "name": {
+                required: true,
+            },
+            "mobile_number_1": {
+                required: true,
+            },
+        },
+        messages: {},
+        errorPlacement: function (error, element) {
+            error.insertAfter(element.parent());
+        },
+        submitHandler: function (form) {
+            let submit_btn = $('button[type="submit"]', form);
+            submit_btn.html(loading_button_html).prop("disabled", true);
+            $.ajax({
+                type: 'PUT',
+                url: _base_url + "masters/delivery-persons",
+                dataType: 'json',
+                headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') },
+                data: edit_delivery_person_form.serialize(),
+                success: function (response) {
+                    if (response.status == true) {
+                        edit_delivery_person_modal.hide();
+                        submit_btn.html('Update').prop("disabled", false);
+                        Swal.fire({
+                            title: response.message.title,
+                            text: response.message.content,
+                            icon: response.message.type,
+                            confirmButtonColor: swal_colors.success_ok,
+                            confirmButtonText: "OK",
+                            allowOutsideClick: false,
+                            didOpen: () => Swal.getConfirmButton().blur()
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                datatable.ajax.reload(null, false);
+                            }
+                        });
+                    } else {
+                        toastStatusFalse(response, { stack: 1 });
+                        submit_btn.html('Update').prop("disabled", false);
+                    }
+                },
+                error: function (response) {
+                    submit_btn.html('Update').prop("disabled", false);
+                    datatable.ajax.reload(null, false);
+                },
+            });
+        }
+    });
+});
