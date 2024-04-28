8d36e360fc2bf121562265a05861664dafba3cdf
Samnad Sainulabdeen <samnads2014@gmail.com>
2024-04-28 15:12:29 +0530

    new changes

--- resources/views/user/includes/sidebar.blade.php
+++ resources/views/user/includes/sidebar.blade.php
@@ -32,8 +32,7 @@
                 </li>
             </ul>
             <ul>
-                <li> <a href="{{ route('user.masters.brands.list') }}"><i
-                            class='bx bx-radio-circle'></i>Brands</a>
+                <li> <a href="{{ route('user.masters.brands.list') }}"><i class='bx bx-radio-circle'></i>Brands</a>
                 </li>
             </ul>
         </li>
@@ -45,46 +44,18 @@
             </a>
         </li>
         <li>
-            <a href="javascript:;" class="has-arrow">
-                <div class="parent-icon"><i class="bx bx-category"></i>
+            <a href="#">
+                <div class="parent-icon"><i class='bx bx-category'></i>
                 </div>
                 <div class="menu-title">Products</div>
             </a>
-            <ul>
-                <li> <a href="{{ route('vendor.product.my-products') }}"><i class='bx bx-radio-circle'></i>My
-                        Products</a>
-                </li>
-                <li> <a href="{{ route('vendor.product.available-products') }}"><i
-                            class='bx bx-radio-circle'></i>Available Products</a>
-                </li>
-            </ul>
         </li>
         <li>
-            <a href="javascript:;" class="has-arrow">
-                <div class="parent-icon"><i class="bx bx-layer-plus"></i>
+            <a href="#">
+                <div class="parent-icon"><i class='bx bx-layer-plus'></i>
                 </div>
                 <div class="menu-title">Product Requests</div>
             </a>
-            <ul>
-                <li> <a href="{{ route('vendor.product.requests') }}"><i class='bx bx-radio-circle'></i>My Requests</a>
-                </li>
-                <li> <a href="{{ route('vendor.product.new-request') }}"><i class='bx bx-radio-circle'></i>New
-                        Request</a>
-                </li>
-            </ul>
-        </li>
-        <li>
-            <a href="javascript:;" class="has-arrow">
-                <div class="parent-icon"><i class="bx bx-cog"></i>
-                </div>
-                <div class="menu-title">Settings</div>
-            </a>
-            <ul>
-                <li> <a href="javascript:;"><i class='bx bx-radio-circle'></i>Profile</a>
-                </li>
-                <li> <a href="javascript:;"><i class='bx bx-radio-circle'></i>Order Settings</a>
-                </li>
-            </ul>
         </li>
     </ul>
     <!--end navigation-->
