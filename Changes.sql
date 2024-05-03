13fb9c8001b8460a4806a51ce77fe4f29f042534
Samnad Sainulabdeen <samnads2014@gmail.com>
2024-05-04 01:37:11 +0530

    new changes

--- Changes.sql
+++ Changes.sql
@@ -193,7 +193,7 @@ CREATE TABLE `vendor_delivery_persons` (
   KEY `vendor_id` (`vendor_id`),
   CONSTRAINT `vendor_delivery_persons_ibfk_1` FOREIGN KEY (`vendor_id`) REFERENCES `vendors` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
----------------------------------------------------------- DONE
+----------------------------------------------------------
 ALTER TABLE `vendors`
 ADD `home_delivery_status_id` tinyint NOT NULL DEFAULT '1' AFTER `blocked_at`;
 
@@ -202,3 +202,92 @@ CHANGE `password` `password` varchar(255) COLLATE 'utf8mb4_unicode_ci' NULL AFTE
 CHANGE `username` `username` varchar(155) COLLATE 'utf8mb4_unicode_ci' NULL AFTER `password`,
 ADD `min_order_value` decimal(10,2) unsigned NOT NULL DEFAULT '100' AFTER `home_delivery_status_id`,
 ADD `min_order_weight` int(11) unsigned NOT NULL DEFAULT '1' AFTER `min_order_value`;
+---------------------------------------------------------- DONE
+ALTER TABLE `products`
+ADD `parent_product_id` bigint(20) unsigned NULL AFTER `id`;
+ALTER TABLE `products`
+ADD FOREIGN KEY (`parent_product_id`) REFERENCES `products` (`id`);
+
+CREATE TABLE `variation_themes` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `variation_theme_name` varchar(180) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `variation_theme_name` (`variation_theme_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `variation_themes` (`id`, `variation_theme_name`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1,	'Size',	'2024-05-04 01:13:35',	'2024-05-04 01:13:35',	NULL);
+
+CREATE TABLE `variants` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `variant_name` varchar(155) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `variant_name` (`variant_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `variants` (`id`, `variant_name`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1,	'Size',	'2024-05-03 23:58:45',	'2024-05-03 23:58:45',	NULL),
+(2,	'Flavour',	'2024-05-04 00:58:39',	'2024-05-04 00:58:39',	NULL);
+
+CREATE TABLE `variant_options` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `variant_id` bigint(20) unsigned NOT NULL,
+  `variant_option_name` varchar(155) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `variant_id_variant_option_name` (`variant_id`,`variant_option_name`),
+  CONSTRAINT `variant_options_ibfk_1` FOREIGN KEY (`variant_id`) REFERENCES `variants` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `variant_options` (`id`, `variant_id`, `variant_option_name`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1,	1,	'1 kg',	'2024-05-04 01:00:18',	'2024-05-04 01:00:18',	NULL),
+(2,	1,	'2 kg',	'2024-05-04 01:00:18',	'2024-05-04 01:00:18',	NULL),
+(3,	1,	'3 kg',	'2024-05-04 01:00:18',	'2024-05-04 01:00:18',	NULL),
+(4,	1,	'4 kg',	'2024-05-04 01:00:18',	'2024-05-04 01:00:18',	NULL),
+(5,	1,	'5 kg',	'2024-05-04 01:00:18',	'2024-05-04 01:00:18',	NULL),
+(6,	1,	'6 kg',	'2024-05-04 01:00:18',	'2024-05-04 01:00:18',	NULL),
+(7,	1,	'7 kg',	'2024-05-04 01:00:18',	'2024-05-04 01:00:18',	NULL),
+(8,	1,	'8 kg',	'2024-05-04 01:00:18',	'2024-05-04 01:00:18',	NULL),
+(9,	1,	'9 kg',	'2024-05-04 01:00:18',	'2024-05-04 01:00:18',	NULL),
+(10,	1,	'10 kg',	'2024-05-04 01:00:18',	'2024-05-04 01:00:18',	NULL),
+(11,	1,	'10 g',	'2024-05-04 01:01:53',	'2024-05-04 01:01:53',	NULL),
+(12,	1,	'25 g',	'2024-05-04 01:01:53',	'2024-05-04 01:01:53',	NULL),
+(13,	1,	'50 g',	'2024-05-04 01:01:53',	'2024-05-04 01:01:53',	NULL),
+(14,	1,	'100 g',	'2024-05-04 01:01:53',	'2024-05-04 01:01:53',	NULL),
+(15,	1,	'150 g',	'2024-05-04 01:01:53',	'2024-05-04 01:01:53',	NULL),
+(16,	1,	'175 g',	'2024-05-04 01:01:53',	'2024-05-04 01:01:53',	NULL),
+(17,	1,	'200 g',	'2024-05-04 01:01:53',	'2024-05-04 01:01:53',	NULL),
+(18,	1,	'250 g',	'2024-05-04 01:01:53',	'2024-05-04 01:01:53',	NULL),
+(19,	1,	'275 g',	'2024-05-04 01:01:53',	'2024-05-04 01:01:53',	NULL),
+(20,	1,	'300 g',	'2024-05-04 01:01:53',	'2024-05-04 01:01:53',	NULL),
+(21,	1,	'400 g',	'2024-05-04 01:01:53',	'2024-05-04 01:01:53',	NULL),
+(22,	1,	'500 g',	'2024-05-04 01:01:53',	'2024-05-04 01:01:53',	NULL),
+(23,	2,	'Chocolate',	'2024-05-04 01:03:30',	'2024-05-04 01:03:30',	NULL),
+(24,	2,	'Strawberry',	'2024-05-04 01:03:30',	'2024-05-04 01:03:30',	NULL),
+(25,	2,	'Pista',	'2024-05-04 01:03:30',	'2024-05-04 01:03:30',	NULL),
+(26,	1,	'100 ml',	'2024-05-04 01:05:03',	'2024-05-04 01:05:03',	NULL),
+(27,	1,	'200 ml',	'2024-05-04 01:05:03',	'2024-05-04 01:05:03',	NULL),
+(28,	1,	'500 ml',	'2024-05-04 01:05:03',	'2024-05-04 01:05:03',	NULL),
+(29,	1,	'650 ml',	'2024-05-04 01:05:03',	'2024-05-04 01:05:03',	NULL);
+
+CREATE TABLE `product_variants` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` bigint(20) unsigned NOT NULL,
+  `variant_option_id` bigint(20) unsigned NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `product_id_variant_option_id` (`product_id`,`variant_option_id`),
+  KEY `variant_option_id` (`variant_option_id`),
+  CONSTRAINT `product_variants_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `product_variants_ibfk_2` FOREIGN KEY (`variant_option_id`) REFERENCES `variant_options` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
