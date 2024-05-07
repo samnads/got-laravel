7b2871848a494b10d3012ce57639f901db83db5a
Samnad Sainulabdeen <samnads2014@gmail.com>
2024-05-08 00:21:09 +0530

    new changes

--- app/Models/VendorDeliveryPerson.php
+++ app/Models/VendorDeliveryPerson.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class VendorDeliveryPerson extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $table = 'vendor_delivery_persons';
 }
