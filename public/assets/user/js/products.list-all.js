f6ad28c74fea57be4aebc76edf29d27d9945a08c
Samnad Sainulabdeen <samnads2014@gmail.com>
2024-05-18 02:37:41 +0530

    Update products.list-all.js

--- public/assets/user/js/products.list-all.js
+++ public/assets/user/js/products.list-all.js
@@ -108,17 +108,17 @@ function format(d) {
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">SKU</th>
+      <th scope="col">Name</th>
+      <th scope="col">Actions</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td>Demo</td>
+      <td>Demo</td>
+      <td>Demo</td>
     </tr>
   </tbody>
 </table>`
